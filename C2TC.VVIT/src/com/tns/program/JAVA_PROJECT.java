3428e7916df7e324525cf9d9aec352a708bbd0cd
maqhn <maqhn@maqhniakhanum>
2023-03-16 21:50:25 +0530

    First commit

---
+++ C2TC.VVIT/src/com/tns/program/JAVA_PROJECT.java
@@ -0,0 +1,10 @@
+package com.tns.program;
+
+public class JAVA_PROJECT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World");
+	}
+
+}
---
+++ C2TC.VVIT/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maqhn
+ *
+ */
+module C2TC.VVIT {
+}
